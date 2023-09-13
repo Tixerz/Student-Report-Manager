c9d85520dd1d500a4c00ee80ca5bd37a5ead215f
Tixerz <petete1296@gmail.com>
2023-09-13 06:33:32 -0700

    better menu + saving objects

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <map>
+
 using namespace std;
 
 class Student{
@@ -9,15 +11,34 @@ class Student{
         int roll_number;
         map<string , int> lesson_grade;
         void create_student();
-        //void get_data();
-        //void calculate_grade();
+        void search();
+        void calculate_grade();
 
 
 };
-
+void show_menu(){
+    system("cls");
+    cout << "------MENU------" << endl << endl ;
+    cout << "1-<Add student>" << endl;
+    cout << "2-<Search student>" << endl ;
+    cout << "3-<Grade calculation>" << endl << endl;
+    string option_txt = "[Choose your option]" ;
+    for(int i = 0 ; i<=option_txt.size() ; i++){
+        cout << option_txt[i] ;
+        _sleep(50) ;
+    }
+    
+    int option ; 
+    cin >> option ;
+    if(option == 1){
+        Student a;
+        a.create_student();
+    }
+}
 void Student::create_student(){
-    map<string ,int> lesson_grade;
-    cout << "Enter student name: " ; 
+    cout << "Enter roll number: " ;
+    cin >> this->roll_number;
+    cout << endl << "Enter student name: " ; 
     cin >> this->name ; 
     cout << endl << "Enter math grade of this student: " ; 
     int mathgrade; 
@@ -27,7 +48,20 @@ void Student::create_student(){
     cin >> chemestrygrade ; 
     this->lesson_grade["math"] = mathgrade ; 
     this->lesson_grade["chemestry"] = chemestrygrade;
+    ofstream file(to_string(this->roll_number)+".txt"); //line1:roll number / line2:name
+    file << to_string(this->roll_number)+"\n" ; 
+    file << this->name+"\n" ; 
+    file << to_string(this->lesson_grade["math"])+"\n" ; 
+    file << to_string(this->lesson_grade["chemestry"])+"\n" ;
+    file.close();
     
-    
+}
+void Student::search(){
+    cout << "Enter roll number: " ; 
+
+}
+int main(){
+
+    show_menu();
 
 }
